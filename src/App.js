40aae0dc71fe57acfcc9516310c750882b14dbdf
Jezebel1990 <jezebelguedes@gmail.com>
2023-05-29 23:12:58 -0300

    create routes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import './App.css';
+import React from 'react';
+
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+
+import Sidebar from './componentes/Sidebar';
+import Header from './componentes/Header';
+import Footer from './componentes/Footer';
 
 function App() {
   return (
-<div className='overflow-hidden'>Ecommerce</div>
+<div className='overflow-hidden'>
+  <Router>
+    <Header />
+    <Routes>
+    <Route path='/' element={<Home />} />
+    <Route path='/product/:id' element={<ProductDetails />} />
+    </Routes>
+    <Sidebar />
+    <Footer />
+  </Router>
+</div>
   );
 }
 

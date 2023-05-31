0466541c32458004972b779e10d11237e678c380
Jezebel1990 <jezebelguedes@gmail.com>
2023-05-31 19:27:59 -0300

    create header

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useContext} from 'react';
+import { SidebarContext } from '../contexts/SidebarContext';
 
-export default function Header() {
-  return (
-    <div>Header</div>
-  )
-}
+import {BsBag} from 'react-icons/bs'
+
+const Header = () => {
+  const {isOpen, setIsOpen} = useContext(SidebarContext);
+return (
+ <header className='bg-blue-300'>
+  <div>Header</div>
+  <div onClick={() => setIsOpen(!isOpen)}
+  className='cursor-pointer flex relative'
+  
+  >
+    <BsBag className= 'text-2xl' />
+   </div>
+</header>
+)
+};
+
+export default  Header;
--- src/index.js
+++ src/index.js
@@ -5,14 +5,17 @@ import App from './App';
 
 import ProductProvider from './contexts/ProductContext';
 
+import SidebarProvider from './contexts/SidebarContext';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+<SidebarProvider>
 <ProductProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>
   </ProductProvider>
+  </SidebarProvider>
 );
 
 
